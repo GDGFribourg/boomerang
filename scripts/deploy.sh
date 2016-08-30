47915302f500301c2f44e8820118d3c509edcd6d
Jacques Supcik <jacques@supcik.net>
2016-08-30 14:50:30 +0200

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 mkdir public
-rsync -a --delete \
-  --exclude node_modules \
-  ./ public/
-
+rsync -a --delete --progress --exclude node_modules ./ public/
 ./node_modules/firebase-tools/bin/firebase deploy --token "$FIREBASE_TOKEN"
